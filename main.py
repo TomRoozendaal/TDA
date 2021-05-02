939deecafd93694a6e329ef788e0dca777979443
Tom van Roozendaal <tomvroozendaal@live.nl>
2021-05-02 16:36:53 +0200

    time based visualization and some data loading functions

--- main.py
+++ main.py
@@ -2,21 +2,90 @@ import read_geolife
 import pandas as pd
 import numpy as np
 import pptk
+import pyproj
+import datetime
+from matplotlib import cm
 
-'''Load/read data'''
-try:
-    df = pd.read_pickle('data/geolife.pkl')
-except (OSError, IOError) as e:
-    df = read_geolife.read_all_users('data')
-    df.to_pickle('data/geolife.pkl')
+'''
+Source: https://heremaps.github.io/pptk/tutorials/viewer/geolife.html
+'''
 
-print(df)
+label_names = {1: 'walk', 2: 'bike', 3: 'bus', 4: 'car', 5: 'subway', 6: 'train', 7: 'airplane', 8: 'boat', 9: 'run',
+               10: 'motorcycle', 11: 'taxi'}
 
-P = np.c_[df['lon'], df['lat'], np.zeros(len(df))]
-viewer = pptk.viewer(P)
+# time colormap
+cols = cm.get_cmap('plasma', 48).colors
+colormap = np.concatenate((cols, cols[::-1]))
 
-'''color each point based on its altitude'''
-viewer.attributes(df['alt'])
+# select a visualization
+modes = {1: 'alt', 2: 'label', 3: 'time'}
+mode = 3
 
-'''give it a colormap; manually set the map domain'''
-viewer.color_map('jet', scale=[0, 20000])
+
+# Load full data frame
+def load_df():
+    try:
+        d = pd.read_pickle('data/geolife.pkl')
+    except (OSError, IOError) as e:
+        d = read_geolife.read_all_users('data')
+        d.to_pickle('data/geolife.pkl')
+    return d
+
+
+# Load data frame masked to UTM zone 50
+def load_df_50():
+    try:
+        d50 = pd.read_pickle('data/geolife_50.pkl')
+    except (OSError, IOError) as e:
+        d = load_df()
+        # Only consider points inside UTM zone 50
+        mask_50 = (d['lon'] > 114.0) & (d['lon'] < 120.0) & (d['lat'] > 32.0) & (d['lat'] < 48.0)
+        d50 = d[mask_50]
+        d50.to_pickle('data/geolife_50.pkl')
+    return d50
+
+
+if mode not in modes:
+    print("mode not available")
+elif modes[mode] == 'alt':
+    '''Load/read data'''
+    df = load_df()
+    print("Done loading data\nSetting up viewer")
+
+    P = np.c_[df['lon'], df['lat'], np.zeros(len(df))]
+    viewer = pptk.viewer(P)
+
+    # Color each point based on its altitude
+    viewer.attributes(df['alt'])
+
+    # Give it a colormap; manually set the map domain
+    viewer.color_map('jet', scale=[0, 20000])
+elif modes[mode] == 'label':
+    '''Load/read data'''
+    df = load_df_50()
+    print("Done loading data\nConverting data")
+
+    # Convert lat/lon to meters in UTM coordinate system
+    # Note: takes time!
+    proj = pyproj.Proj(proj='utm', zone=50, ellps='WGS84')
+    x, y = proj(df['lon'].tolist(), df['lat'].tolist())
+    P = np.c_[x, y, 0.3048 * df['alt']]  # feet are converted to meters
+    print("Conversion done\nSetting up viewer")
+
+    # Setup viewer; only show labelled data
+    print(label_names)
+    mask_labelled = df['label'] != 0
+    viewer = pptk.viewer(P[mask_labelled])
+    viewer.attributes(df[mask_labelled]['label'])
+elif modes[mode] == 'time':
+    '''Load/read data'''
+    df = load_df_50()
+    print("Done loading data\nSetting up viewer")
+
+    P = np.c_[df['lon'], df['lat'], np.zeros(len(df))]
+    hours = df['time'].dt.hour + round(df['time'].dt.minute / 15) * 0.25
+
+    # Setup viewer; only show labelled data
+    viewer = pptk.viewer(P)
+    viewer.attributes(hours)
+    viewer.color_map(colormap)
